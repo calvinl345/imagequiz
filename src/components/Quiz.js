797427643280278e4450fbdadfb4834c41c56cc2
Calvin Smith <calvinl345@gmail.com>
2021-10-19 17:01:00 -0600

    added shuffle

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -50,6 +50,26 @@ const Quiz = (props) => {
         history.push({ pathname: "/" });
     };
 
+    let shuffle = (array) => {
+        let currentIndex = array.length,
+            randomIndex;
+
+        // While there remain elements to shuffle...
+        while (currentIndex != 0) {
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+
+            // And swap it with the current element.
+            [array[currentIndex], array[randomIndex]] = [
+                array[randomIndex],
+                array[currentIndex],
+            ];
+        }
+
+        return array;
+    };
+
     useEffect(() => {
         setCustomer(localStorage.getItem("customer"));
     });
@@ -67,7 +87,7 @@ const Quiz = (props) => {
                     <Row>
                         <Col>
                             <ListGroup>
-                                {questions[currentIndex].choices.map(
+                                {shuffle(questions[currentIndex].choices).map(
                                     (choice) => (
                                         <ListGroup.Item
                                             key={choice}
